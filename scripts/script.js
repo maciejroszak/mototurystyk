f5f63aa15ab354d2cd82c1d0c9b9c247050038c6
maciejroszak <roszak.maciej4@gmail.com>
2020-01-30 18:44:28 +0100

    Kopia 30-01-2020

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+function contactCheckbox() {
+    let btn = document.getElementsByClassName("submit");
+    var onClick = 
+    if (zmienna == false) {
+            alert("Wymagane zaznaczenie.");
+        }
+}
